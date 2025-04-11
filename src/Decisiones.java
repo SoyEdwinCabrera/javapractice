74eb8c47861e1eb035ae1c8a525d4c7e63a823cd
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-04-10 22:21:43 -0500

    Tomando decisiones #2

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -2,13 +2,21 @@ public class Decisiones {
 
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
 
+//      Código que prueba los operadores relacionales
         if(fechaDeLanzamiento > 2022) {
             System.out.println("Péliculas más populares");
         } else {
             System.out.println("Película Retro que aún vale la pena ver.");
+        };
+
+        if(incluidoEnElPlan && tipoPlan.equals("plus")) {
+            System.out.println("Puedes ver la película.");
+        } else {
+            System.out.println("No puedes ver la película.");
         };
     }
 }
