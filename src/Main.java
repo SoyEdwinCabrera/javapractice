c1a2ba7714d1baf4ad58ec841513b0b542344b68
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-04-10 21:29:02 -0500

    Trabajando con textos

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
 
     double media = (8.2 + 6.0 + 9.0)/3;
     System.out.println("La media de las calificaciones es: " + media);
+
+    String sinopsis = """
+            Un hacker llamado Neo investiga sobre una misteriosa realidad 
+            llamada Matrix y se une a un grupo de rebeldes que luchan 
+            contra las máquinas que dominan el mundo.
+            Fue lanzada en el año:
+            """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+
     }
 }
