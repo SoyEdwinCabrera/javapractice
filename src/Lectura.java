7cd01c3d4821831a683466ced7f07c56d16a3a7f
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-04-10 22:35:10 -0500

    Lectura de datos

---
+++ src/Lectura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lectura {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el nombre de tu película favorita:");
+        String pelicula = scanner.nextLine();
+        System.out.println("Escribe el año de lanzamiento:");
+        int fechaDeLanzamiento = scanner.nextInt();
+        System.out.println("Escribe la calificación de la película:");
+        double nota = scanner.nextDouble();
+
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(nota);
+    }
+}
