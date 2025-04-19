8ece7285405e788a9dcf9eb196d3123cc2e70399
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-04-18 22:04:21 -0500

    Modelando las clases de la aplicacion

---
+++ src/Challenge_3/TarjetaDeCredito.java
@@ -0,0 +1,37 @@
+package Challenge_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarjetaDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> listaDeCompras;
+
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = saldo;
+        this.listaDeCompras = new ArrayList<>();
+    }
+
+    public boolean lanzarCompra(Compra compra) {
+        if (this.saldo >= compra.getValor()) {
+            this.saldo -= compra.getValor();
+            listaDeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListaDeCompras() {
+        return listaDeCompras;
+    }
+}
